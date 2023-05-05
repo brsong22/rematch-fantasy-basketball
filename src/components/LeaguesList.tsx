e92e3886aba4242d48e61434d849b08ce548f7b6
Richard Song <brsong22@gmail.com>
2023-05-05 11:28:00 -0500

    fetch and display additional team data
    - add functionality to the LeaguesList component to allow user to click a list item to view additional data for the league
    - start off just showing the user's team's placing in the league
    - utilize useState() to set state for the component so we can store data to display
    - when the user clicks a list item initiate request to yahoo's api to get the league's standings which will include teams data where we can loop through to find the user's specific team
    - it doesn't seem that yahoo has an endpoint to allow us to specifically get a user's team for a given league
    
    to do/consider:
    - use caching to reduce requests to yahoo. we can probably utilize sessionStorage for client side caching since league data should not change often/at all especially for leagues that have concluded
    - break out LeaguesList into multiple components so that we can separate out functionality and be more modular
    - consider defining type interfaces for common (yahoo) resources

--- src/components/LeaguesList.tsx
+++ src/components/LeaguesList.tsx
@@ -1,23 +1,25 @@
 import React, { FunctionComponent, useEffect, useMemo, useState } from 'react'
-import { useSession } from 'next-auth/react'
+import { useSession, SessionData } from 'next-auth/react'
 import Link from 'next/link'
 
 const LeaguesList: FunctionComponent = () => {
-	const [leagues, setLeagues] = useState([])
-	const { data: session } = useSession()
+	const [leagues, setLeagues] = useState<array>([])
+	const [selectedLeague, setSelectedLeague] = useState<string | null>(null)
+	const [userGuid, setUserGuid] = useState<string | null>(null)
+	const [userTeam, setUserTeam] = useState(null)
+	const [leagueTeams, setLeagueTeams] = useState([])
+
+	const { data: session } = useSession<SessionData | null>()
+	const accessToken = session.token.accessToken
+
+	// https://cors-anywhere.herokuapp.com/corsdemo to enable corsanywhere
+	const corsAnywhere = process.env.NEXT_PUBLIC_LOCAL_DEV ? process.env.NEXT_PUBLIC_CORS_ANYWHERE : ``
 
 	useEffect(() => {
 		const fetchLeagues = async () => {
-			const accessToken = session.token.accessToken
-			if (!accessToken) {
-				return
-			}
+			const gamesUrl = `${corsAnywhere}https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_codes=nba/leagues?format=json`
 
-			// https://cors-anywhere.herokuapp.com/corsdemo to enable corsanywhere
-			const corsAnywhere = process.env.NEXT_PUBLIC_LOCAL_DEV ? process.env.NEXT_PUBLIC_CORS_ANYWHERE : ``
-			const userFantasyNbaGames = `${corsAnywhere}https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_codes=nba/leagues?format=json`
-
-			const response = await fetch(userFantasyNbaGames, {
+			const response = await fetch(gamesUrl, {
 				headers: {
 					Authorization: `Bearer ${accessToken}`,
 				},
@@ -25,21 +27,103 @@ const LeaguesList: FunctionComponent = () => {
 
 			if (response.ok) {
 				const data = await response.json()
-				const games = Object.values(data.fantasy_content.users[0].user[1].games).slice(0, -1)
-				const leagues = games.map((game) => {
-					return Object.values(game.game[1].leagues).slice(0, -1).map((league) => ({
-						key: league.league[0].league_key,
-						name: league.league[0].name,
-						season: league.league[0].season,
-					}))
-				})
+				const gamesData = data.fantasy_content.users[0]
 
-				setLeagues(leagues.flat())
+				const userGuid = gamesData.user[0].guid
+				setUserGuid(userGuid)
+
+				const games = Object.values(gamesData.user[1].games).slice(0, -1)
+				const leaguesArray = games
+					.map((gameNum) => (Object.values(gameNum).flat()[1].leagues))
+					.flatMap((objectOfLeagues) => (Object.keys(objectOfLeagues).slice(0, -1).map((objectKey) => (objectOfLeagues[objectKey]))))
+					.map((league) => league.league[0])
+				setLeagues(leaguesArray)
 			}
 		}
 
 		fetchLeagues()
 	}, [session])
+
+	const handleClickedLeague = (e: MouseEvent<HTMLAnchorElement>, league) => {
+		e.preventDefault();
+
+		const {
+			end_date: endDate,
+			end_week: endWeek,
+			is_finished: isFinished,
+			league_id: leagueId,
+			league_key: leagueKey,
+			name,
+			num_teams: leagueSize,
+			season,
+			start_date: startDate,
+			start_week: startWeek,
+			url
+		}: {
+			endDate: string,
+			endWeek: number,
+			isFinished: boolean,
+			leagueId: number,
+			leagueKey: string,
+			name: string,
+			leagueSize: number,
+			season: number,
+			startDate: string,
+			startWeek: number,
+			url: string
+		} = {
+			...league,
+			endWeek: parseInt(league.end_week),
+			isFinished: Boolean(league.is_finished),
+			leagueId: parseInt(league.league_id),
+			leagueSize: parseInt(league.num_teams),
+			season: parseInt(league.season),
+			startWeek: parseInt(league.start_week)
+		}
+		if (leagueKey === selectedLeague) {
+			return
+		}
+		setSelectedLeague(leagueKey)
+
+		const fetchTeams = async () => {
+			const standingsUrl = `${corsAnywhere}https://fantasysports.yahooapis.com/fantasy/v2/league/${leagueKey}/standings?format=json`
+			
+			const response = await fetch(standingsUrl, {
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			})
+
+			if (response.ok) {
+				const data = await response.json()
+				const teamsObjects = data.fantasy_content.league[1].standings[0]
+				const teamsArray = Object.values(teamsObjects.teams)
+					.slice(0, -1)
+					.map((team) => (team.team))
+					.map((team) => ({
+						info: team[0].reduce((teamInfo, infoItem) => Object.assign(teamInfo, infoItem), {}),
+						stats: team[1],
+						standings: team[2].team_standings,
+					})
+				)
+				setLeagueTeams(teamsArray)
+
+				const foundTeam = teamsArray.find((team) => {
+					const teamManagers = team.info.managers
+					const manager = teamManagers.find((manager) => {
+						return manager.manager.guid === userGuid
+					})
+
+					return manager
+				})
+
+				setUserTeam(foundTeam)
+			}
+		}
+		fetchTeams()
+
+		fetchLeagues()
+	}
 
 	return (
 		<>
@@ -51,13 +135,21 @@ const LeaguesList: FunctionComponent = () => {
 				<ul>
 					{leagues.map((league, index) => (
 						<li key={index}>
-							<strong>{league.season}:</strong>
-							<strong>{league.season}:&nbsp;</strong>
-								{league.name}
+							<a href="" onClick={(e) => handleClickedLeague(e, league)}><strong>{league.season}:&nbsp;</strong>{league.name}</a>
 						</li>
 					))}
 				</ul>
 		 	</div>
+		 	{userTeam
+		 		&& <>
+		 			<span>---</span>
+		 			<br />
+		 			{userTeam.info.name} <strong>placed {userTeam.standings.rank}/{leagueTeams.length}</strong>
+		 			<br />
+		 			<span>---</span>
+		 			<br />
+		 		</>
+		 	}
 		</>
 	)
 }
